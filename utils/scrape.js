e2ada35117c37a369839516fb4144e12176e4330
Faris-scraper <rissstore47@gmail.com>
2025-05-18 22:14:12 +0700

    Update scrape.js

--- utils/scrape.js
+++ utils/scrape.js
@@ -1,3 +1,34 @@
-const axios = require("axios")
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const clean = e => (e = e.replace(/(<br?\s?\/>)/gi, " \n")).replace(/(<([^>] )>)/gi, "");
+
+async function shortener(e) {
+  return e;
+}
 
 global.creator = "Faris"
+
+async function tiktok(url) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let t = await axios("https://lovetik.com/api/ajax/search", { method: "post", data: new URLSearchParams(Object.entries({ query: url })) });
+
+      const result = {};
+      result.title = clean(t.data.desc);
+      result.author = clean(t.data.author);
+      result.nowm = await shortener((t.data.links[0].a || "").replace("https", "http"));
+      result.watermark = await shortener((t.data.links[1].a || "").replace("https", "http"));
+      result.audio = await shortener((t.data.links[2].a || "").replace("https", "http"));
+      result.thumbnail = await shortener(t.data.cover);
+
+      resolve(result);
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+
+module.exports = {
+  tiktok
